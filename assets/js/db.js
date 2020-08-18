60cc072a15fb0be049bf38387ae5e474227d34a9
Cleidson SIlva <contatosilva062001@gmail.com>
2020-08-17 23:39:35 -0300

    Adicionado db-json e script

---
+++ assets/js/db.js
@@ -0,0 +1,11 @@
+let mercadoJson = [
+    
+    {id:1, name:'Coca Cola', img:'assets/img/coca-cola.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Bem Geleda'},
+    {id:1, name:'Coca Cola', img:'assets/img/coca-cola.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Bem Geleda'},
+    {id:1, name:'Coca Cola', img:'assets/img/coca-cola.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Bem Geleda'},
+    {id:1, name:'Coca Cola', img:'assets/img/coca-cola.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Bem Geleda'},
+    {id:1, name:'Coca Cola', img:'assets/img/coca-cola.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Bem Geleda'},
+    {id:1, name:'Coca Cola', img:'assets/img/coca-cola.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Bem Geleda'}
+    
+   
+];
