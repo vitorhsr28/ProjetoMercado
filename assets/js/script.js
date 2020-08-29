c2df94f6b541a5de57c1f3a452bb47c15f9920b5
Cleidson Silva <contatosilva06200@gmail.com>
2020-08-28 23:00:07 -0300

    Atualizando Script add forEatch

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,67 +10,75 @@ const cs = el => document.querySelectorAll(el);
 
 axios.get('http://localhost:3000/mercado')
 
-.then(res => { 
-    //Resultado da Api
-    let produtos = (res.data)
-    //Anda no objeto que recebe da api
-    for(let i = 0; i <= produtos.length; i++){
-        //puxando o a posição do array
-        let produtosApi = produtos[i]
-        // array vazio
-        let formatado = []
-       // enviando as informações que vem do array como objeto para um array vazio pois o .map logo abaixo
-       //só funciona em array
-       formatado.push(produtosApi)
-      //listando o array
-       formatado.map((item, index) =>{
-           //O método cloneNode() duplica um elemento node da estrutura de um documento DOM.
-           // Ele retorna um clone do elemento para o qual foi invocado.
-           let mercadoItem = c('.models .mercado-item').cloneNode(true);
-           //Adiciona um novo atributo ou modifica o valor de um atributo existente num elemento específico.
-            mercadoItem.setAttribute('data-key', index);
-            //Retorna o primeiro elemento descendente do elemento em que a função foi invocada e que corresponde aos seletores especificado
-            mercadoItem.querySelector('.mercado-item--name').innerHTML = item.name
-            mercadoItem.querySelector('.mercado-item--price').innerHTML = `R$: ${item.price.toFixed(2)}`   
-            mercadoItem.querySelector('.mercado-item--desc').innerHTML = item.description
-            mercadoItem.querySelector('.mercado-item--img img').src = item.img
-            //addEventListener() registra uma única espera de evento em um único alvo
-            mercadoItem.querySelector('.mercado-link').addEventListener('click', (e)=>{
-                e.preventDefault();
+    .then(res => {
+        //Resultado da Api
+        let produtos = (res.data)
+        //Anda no objeto que recebe da api
+        for (let i = 0; i <= produtos.length; i++) {
+            //puxando o a posição do array
+            let produtosApi = produtos[i]
+            // array vazio
+            let formatado = []
+            // enviando as informações que vem do array como objeto para um array vazio pois o .map logo abaixo
+            //só funciona em array
+            formatado.push(produtosApi)
+            //listando o array
+            formatado.forEach(() => {
 
-                modalQt = 1;
+                formatado.map((item, index) => {
+                    //O método cloneNode() duplica um elemento node da estrutura de um documento DOM.
+                    // Ele retorna um clone do elemento para o qual foi invocado.
+                    let mercadoItem = c('.models .mercado-item').cloneNode(true);
+                    //Adiciona um novo atributo ou modifica o valor de um atributo existente num elemento específico.
+                    mercadoItem.setAttribute('data-key', index);
+                    //Retorna o primeiro elemento descendente do elemento em que a função foi invocada e que corresponde aos seletores especificado
+                    mercadoItem.querySelector('.mercado-item--name').innerHTML = item.name
+                    mercadoItem.querySelector('.mercado-item--price').innerHTML = `R$: ${item.price.toFixed(2)}`
+                    mercadoItem.querySelector('.mercado-item--desc').innerHTML = item.description
+                    mercadoItem.querySelector('.mercado-item--img img').src = item.img
+                    //addEventListener() registra uma única espera de evento em um único alvo
+                    mercadoItem.querySelector('.mercado-link').addEventListener('click', (e) => {
+                        e.preventDefault();
 
-               let key = e.target.closest('.mercado-item').getAttribute('data-key')
+                        modalQt = 1;
+                        //getAttribute() retorna o valor de um argumento específico do elemento.
+                        // Se o atributo não existir, o valor retornado será null ou "" (string vazia).
+                        
+                        let key = e.target.closest('.mercado-item').getAttribute('data-key')
 
-               c('.mercadoInfo h1').innerHTML = formatado[key].name;
-               c('.mercadoInfo--desc').innerHTML = formatado[key].description
-               c('.mercadoBig img').src = formatado [key].img;
-               c('.mercadoInfo--actualPrice').innerHTML = `R$: ${formatado[key].price.toFixed(2)}` 
+                        c('.mercadoInfo h1').innerHTML = formatado[key].name;
+                        c('.mercadoInfo--desc').innerHTML = formatado[key].description
+                        c('.mercadoBig img').src = formatado[key].img;
+                        c('.mercadoInfo--actualPrice').innerHTML = `R$: ${formatado[key].price.toFixed(2)}`
 
-               c('.mercadoInfo--qt').innerHTML = modalQt;
-               
-                
+                        c('.mercadoInfo--qt').innerHTML = modalQt;
+                        c('.mercadoWindowArea').style.opacity = 0;
+                        c('.mercadoWindowArea').style.display = 'flex';
+                        setTimeout(() => {
+                            c('.mercadoWindowArea').style.opacity = 1;
 
-                c('.mercadoWindowArea').style.opacity = 0;
-                c('.mercadoWindowArea').style.display= 'flex';
-                setTimeout(()=>{
-                    c('.mercadoWindowArea').style.opacity = 1;
+                        }, 200)
 
-                },200)
-              
-            });
-            c('.mercado-area').append(mercadoItem);
-       })
-    }
- })
- .catch(err => {
-    console.log('Erro => ' +err)
-})
+                    });
+                    c('.mercado-area').append(mercadoItem);
+                })
+
+            })
+
+
+        }
+    })
+
+
+
+    .catch(err => {
+        console.log('Erro => ' + err)
+    })
 // Fim Listagem Mercado
 
 // Começo Modal
 
-function closeModal(){
+function closeModal() {
     c('.mercadoWindowArea').style.opacity = 0;
     setTimeout(() => {
         c('.mercadoWindowArea').style.display = 'none';
@@ -83,17 +91,17 @@ cs('.mercadoInfo--cancelButton, .mercadoInfo--cancelMobileButton').forEach((item
 });
 
 
-c('.mercadoInfo--qtmais').addEventListener('click', ()=>{
+c('.mercadoInfo--qtmais').addEventListener('click', () => {
     modalQt++;
     c('.mercadoInfo--qt').innerHTML = modalQt;
-   
-   
+
+
 
 })
 
-c('.mercadoInfo--qtmenos').addEventListener('click', ()=>{
-   
-     if(modalQt > 1){
+c('.mercadoInfo--qtmenos').addEventListener('click', () => {
+
+    if (modalQt > 1) {
         modalQt--;
         c('.mercadoInfo--qt').innerHTML = modalQt;
     }
@@ -105,7 +113,6 @@ c('.mercadoInfo--qtmenos').addEventListener('click', ()=>{
 
 
 
- 
 
 
 
@@ -121,27 +128,27 @@ c('.mercadoInfo--qtmenos').addEventListener('click', ()=>{
 
 
 
- const url = "​https://api.postmon.com.br/v1/cep/"
- 
- let botao = document.getElementById("buscaCEP")
 
-botao.addEventListener('click', ()=> {
+const url = "​https://api.postmon.com.br/v1/cep/"
+
+let botao = document.getElementById("buscaCEP")
+
+botao.addEventListener('click', () => {
     let cepDigitado = document.getElementById("cep")
     buscaEndereco(cepDigitado.value)
 })
 
- function buscaEndereco(cep){
-     console.log("Buscando informações do endereço para o cep: " + cep + "...")
- 
-     const urlComCep = `https://api.postmon.com.br/v1/cep/${cep}`
- axios.get(urlComCep)
-     .then(res => {
-         console.log(res)
-         let frete = res.data.bairro === "Jardim Sônia Maria" ? 1 : 1.2
-         console.log(frete * 25)
-     })
-     .catch(err => {
-         alert('Esse CEP nao existe' + err)
-     })
- }
+function buscaEndereco(cep) {
+    console.log("Buscando informações do endereço para o cep: " + cep + "...")
 
+    const urlComCep = `https://api.postmon.com.br/v1/cep/${cep}`
+    axios.get(urlComCep)
+        .then(res => {
+            console.log(res)
+            let frete = res.data.bairro === "Jardim Sônia Maria" ? 1 : 1.2
+            console.log(frete * 25)
+        })
+        .catch(err => {
+            alert('Esse CEP nao existe' + err)
+        })
+}
